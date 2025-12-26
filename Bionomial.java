bf7cd7dfc2d3748250a5bcd7d609ea711796891c
Samiullah Alam <asamiullah084@gmail.com>
2025-12-26 10:24:26 +0530

    initial add

---
+++ Bionomial.java
@@ -0,0 +1,21 @@
+public class Bionomial {
+    public static int factorial(int n) {
+        int fact = 1;
+        for(int i=1;i<=n;i++){
+            fact = fact*i;
+        }
+        return fact;
+    }
+    public static int BinCoeff(int n, int r) {
+        int n_fact = factorial(n);
+        int r_fact = factorial(r);
+        int nmr_fact = factorial(n-r);
+
+        int BinCoeff = n_fact/(r_fact*nmr_fact);
+        return BinCoeff;
+    }
+    public static void main(String[] args) {
+        System.out.println(BinCoeff(5, 4));
+        System.out.println(factorial(5));
+    }
+}
